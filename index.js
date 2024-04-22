1dbe493b99306843c48a39a18f7b19a2d9904e0a
nudleee <79415098+nudleee@users.noreply.github.com>
2024-04-22 21:11:56 +0200

    inspect error

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.get("/files", async (_, res) => {
 
     res.status(200).json(descFiles);
   } catch (error) {
+    console.log(error);
     res.status(500).send("Error fetching files");
   }
 });
